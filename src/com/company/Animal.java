4a517136a21212cb2241fa0fc943659f13520482
meczutada <amechut@gmail.com>
2020-05-05 16:08:10 +0200

    Task 1 - Pets feeding process

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,4 +1,44 @@
 package com.company;
 
 public class Animal {
+    final String species;
+    private Double weight;
+
+    public Animal(String species) {
+        this.species = species;
+        switch (species) {
+            case "dog":
+                this.weight = 15.0;
+                break;
+            case "cat":
+                this.weight = 3.5;
+                break;
+            case "myszojeleń":
+                this.weight = 2.4;
+                break;
+            default:
+                this.weight = 1.0;
+                break;
+        }
+    }
+
+    private boolean isAlive() {
+        return this.weight > 0;
+    }
+
+    public void feed() {
+        if (isAlive()) {
+            this.weight++;
+            System.out.println("Sweet! Now I weigh " + this.weight + " kg");
+        }
+        else System.out.println("You starved him!");
+    }
+
+    public void takeForAWalk() {
+        if (isAlive()) {
+            this.weight--;
+            System.out.println("There is nothing like cardio. My weight has dropped to " + this.weight + " kg");
+        }
+        else System.out.println("Now you're the one who can walk to the cemetery.");
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,43 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Human me = new Human();
+
+        me.firstName = "Adrianna";
+        me.lastName = "Mechut";
+        me.pet = new Animal("dog");
+
+        me.pet.feed();
+        me.pet.takeForAWalk();
+        me.pet.feed();
+        me.pet.takeForAWalk();
+        me.pet.feed();
+        me.pet.feed();
+        me.pet.feed();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.feed();
+        me.pet.feed();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
+        me.pet.feed();
+        me.pet.takeForAWalk();
+        me.pet.takeForAWalk();
     }
 }
